d34eb0f8a0c64e02b35a34348ac9dd2de3ed80f8
dsstest01 <test@decision-software.com>
2022-08-07 22:34:27 -0400

    Excel

--- main.py
+++ main.py
@@ -3,8 +3,8 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-import openpyxl as xl
-import os,sys,xlsx
+import os,sys
+import xlsx
 
 #Main Program
 Infile = 'transactions.xlsx'
